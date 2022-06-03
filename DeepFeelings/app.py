01b0273a8a2d6011897fd7465b161f88f3c9939b
valeria mignolo <mignolovaleria@gmail.com>
2022-06-03 14:18:54 +0200

    Front end for first presentation

--- DeepFeelings/app.py
+++ DeepFeelings/app.py
@@ -4,35 +4,39 @@ import numpy as np
 import pandas as pd
 import emoji
 import plotly.express as px
+from PIL import Image
 from data import get_data
-from visualization import pie_chart, timeline_chart, bar_chart
+from visualization import pie_chart, timeline_chart, word_cloud
 
 df2 = get_data()
 fig1 = pie_chart()
-fig2 = bar_chart()
+fig2 = word_cloud()
 fig3 = timeline_chart()
 
 # page config
 # Use the full page instead of a narrow central column
 st.set_page_config(layout="wide")
 
-#title
+# title image
+title_image = Image.open("/home/lewagonvaleria/code/mfcorredor/DeepFeelings/DeepFeelings/images/deep-learning-large.jpg")
+st.image(title_image)
+
+#titles
 st.markdown("""# DeepFeelings
 ## Brand Sentiment Analysis
 """)
-#sidebar title
-with st.sidebar:
-    st.sidebar.write("**Check out what people is thinking about your brand and products!**")
-    st.text_input('Brand')
-    st.text_input('Product')
+st.write("**Check out what people is thinking about your brand and products!**")
+st.text_input('Insert the name of your brand')
+st.text_input('Insert the name of your product')
 
 # linecount
 line_count = st.slider('Select a line count', 1, 10, 3)
 head_df2 = df2.head(line_count)
 head_df2
 
+
 # buttons to dispay numbers (number of reviews, number of positive reviews, etc.)
-st.button("Hola")
+
 
 #columns creation
 columns = st.columns(2)
@@ -43,6 +47,19 @@ st.markdown("""
 """)
 st.markdown("""
 """)
-second_column = columns[1].pyplot(fig2)
+
+with columns[1]:
+    st.markdown("""# Most common words used in the reviews or tweets""")
+    second_column = columns[1].pyplot(fig2)
 
 st.pyplot(fig3)
+
+CSS="""
+    img{
+        width:200vh;
+    }
+    .css-ocqkz7{
+        margin-top:100px
+    }
+"""
+st.write(f'<style>{CSS}</style>', unsafe_allow_html=True)
--- DeepFeelings/data.py
+++ DeepFeelings/data.py
@@ -4,12 +4,13 @@ import pandas as pd
 
 
 def get_data():
+    '''get the the data from amazon scraped reviews'''
 
     df2 = pd.read_csv('/home/lewagonvaleria/code/mfcorredor/DeepFeelings/raw_data/example_amz_rev.csv')
     df2 = df2[['texts', 'date','polarity']]
     #replace polarity numbers with words, sort date column date
     df2["polarity"].replace([2,1,0],['Positive','Neutral','Negative'], inplace=True)
-    df2 = df2[['date','polarity']]
+    df2 = df2[['texts', 'date','polarity']]
     df2.sort_values(by = ['date'], inplace=True)
     df2['date']= pd.to_datetime(df2['date'])
 
