ab8422af627948cb4714b9134d0e4c7a46dd5004
Jannick Preiwisch <jannick@preiwisch.de>
2022-06-06 17:24:35 +0200

    finally made everything work again

--- DeepFeelings/cluster_viz.py
+++ DeepFeelings/cluster_viz.py
@@ -38,7 +38,7 @@ def remove_stop(text_list):
 
 #preprocess to get most important words in main df column or just as a list
 def get_important_words(df, list_output = False):
-    df["important_words"] = df["texts"].apply(lambda x: x.split(" "))
+    df["important_words"] = df["text"].apply(lambda x: x.split(" "))
     df["important_words"] = df["important_words"].apply(lambda x: lemmatize(x))
     df["important_words"] = df["important_words"].apply(lambda x: remove_stop(x))
     df["important_words"] = df["important_words"].apply(lambda x: " ".join(x))
@@ -47,7 +47,7 @@ def get_important_words(df, list_output = False):
 
     if list_output:
         temp_df = pd.DataFrame()
-        temp_df["texts"] = df["important_words"]
+        temp_df["text"] = df["important_words"]
 
         list_of_lists_for_each_text = temp_df.values.tolist()
         list_of_strings_for_each_text = []
@@ -63,7 +63,7 @@ def get_important_words(df, list_output = False):
 
 def get_list_of_strings(df):
     temp_df = pd.DataFrame()
-    temp_df["texts"] = df["texts"]
+    temp_df["text"] = df["text"]
 
     list_of_lists_for_each_text = temp_df.values.tolist()
     list_of_strings_for_each_text = []
@@ -151,7 +151,7 @@ def get_clusters_plotted(df, default_on_comments =True, default_w_stopwords = Tr
         x="x",
         y="y",
         color="clusters",
-        hover_name="texts",
+        hover_name="text",
         )
 
         result = fig.show()
--- DeepFeelings/preproc.py
+++ DeepFeelings/preproc.py
@@ -2,13 +2,30 @@ import pandas as pd
 import re
 import string
 import numpy as np
-from sentiment_analysis import get_data_sentiment_model
 import emoji
 from nltk.corpus import words
+from amzwbscr_selenium import scrape_amz
+from twitter_api import get_lastest_tweets
 
 def remove_emoji(text):
     return emoji.get_emoji_regexp().sub(u'', text)
 
+def get_data_sentiment_model(ls_product_id, user_name, n_tweets=100):
+    '''Get the data from the twitter api and amazon web scraping and join to a df.
+        Returns a df with 3 columns: country, dates and text with the tweets and amazon product reviews.
+        ls_product_id = is a list of amazon product ids of the enterprise
+        user_name = the user name of the enterprice on twitter
+        n_tweets = number of tweets to retrieve (note: it will return at least n_tweets
+        but it can return around 10 more'''
+
+    df = pd.DataFrame()
+
+    for i in ls_product_id:
+        product_reviews = scrape_amz(i)
+        df = pd.concat([df,product_reviews])
+
+    tweets = get_lastest_tweets(user_name, n_tweets)
+    return pd.concat([df,tweets])
 
 def preproc(ls_product_id, user_name, n_tweets=100):
 
