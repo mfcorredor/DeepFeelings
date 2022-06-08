68ed2a5a846820649fdd2a3180c7b6a1a18f1092
valeria mignolo <mignolovaleria@gmail.com>
2022-06-08 12:35:13 +0200

    Front end modifications with video

--- DeepFeelings/app.py
+++ DeepFeelings/app.py
@@ -8,78 +8,74 @@ from PIL import Image
 from visualization import pie_chart, timeline_chart, word_cloud
 #from sentiment_analysis import get_sentiment
 from LDA_clustering import get_topics_LDA_model, preproc_LDA
+import base64
 
-
-# page config
-# Use the full page instead of a narrow central column
+# page config, use the full page instead of a narrow central column
 st.set_page_config(layout="wide")
 
-#video background
-video_html = """
-		<style>
 
-		#myVideo {
-		  position: fixed;
-		  right: 0;
-		  bottom: 0;
-		  min-width: 100%;
-		  min-height: 100%;
-		}
+#background
+def set_bg_hack(main_bg):
+    '''
+    A function to unpack an image from root folder and set as bg.
+    Returns
+    -------
+    The background.
+    '''
+    # set bg name
+    main_bg_ext = "gif"
+    st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()});
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
 
-		.content {
-		  position: fixed;
-		  bottom: 0;
-		  background: rgba(0, 0, 0, 0.5);
-		  color: #f1f1f1;
-		  width: 100%;
-		  padding: 20px;
-		}
-
-		</style>
-		<video autoplay muted loop id="myVideo">
-		  <source src="https://www.youtube.com/watch?v=j22DmsZEv30")>
-		  Your browser does not support HTML5 video.
-		</video>
-        <video controls>
-
-
-"""
-
-st.markdown(video_html, unsafe_allow_html=True)
+set_bg_hack('../raw_data/video_20_sec.gif')
 
 # title image
-title_image = Image.open("images/deep-learning-large (1).jpg")
-st.image(title_image)
+#title_image = Image.open("images/deep-learning-large (1).jpg")
+#st.image(title_image)
 
 #titles and input buttons
-new_title = '<p style="font-family:sans-serif; color:#C6CDD4; font-size: 25px;">Check out what people is thinking about your brand and products!</p>'
-input_1 = '<p style="font-family:sans-serif; color:#C6CDD4; font-size: 20px;">Insert your product ID</p>'
-input_2 = '<p style="font-family:sans-serif; color:#C6CDD4; font-size: 20px;">Insert the name of your brand</p>'
+new_title = '<p style="font-family:sans-serif; font-weight:bold; color:white; font-size: 120px;">DeepFeelings</p>'
+input_1 = '<p style="font-family:sans-serif; font-weight:bold; color:white; font-size: 25px;">Insert product name</p>'
+input_2 = '<p style="font-family:sans-serif; font-weight:bold; color:white; font-size: 25px;">Insert brand name</p>'
 st.markdown(new_title, unsafe_allow_html=True)
-st.write("**  **")
+st.write(" ")
 st.markdown(input_1, unsafe_allow_html=True)
 product = st.text_input('')
 st.write(" ")
 st.markdown(input_2, unsafe_allow_html=True)
 brand = st.text_input(' ')
 
-topics_neg_sneakers = 'look wear comfortable fake small little returned size fit feel tight quality'
+#topics_neg_sneakers = 'look wear comfortable fake small little returned size fit feel tight quality'
+topics = pd.read_csv('../raw_data/topics_neg.csv', index_col='brand_product')
 
 if st.button("Get report"):
-
+    brand = brand.lower()
+    product = product.lower()
     if brand != '' and product != '':
-        file_list = ["../raw_data/sentiment_amz_{product}.csv",
-                    "../raw_data/sentiment_tw_{brand}.csv"]
+        file_list = [f"../raw_data/sentiment_amz_{product}.csv",
+                    f"../raw_data/sentiment_tw_{brand}.csv"]
         csv_list = [pd.read_csv(file) for file in file_list]
         data = pd.concat(csv_list, ignore_index=True)
         data_amz = pd.read_csv(file_list[0])
         data_amz['date'] = pd.to_datetime(data['date'])
+        topic = topics['negative_topics'][brand]
         fig3 = timeline_chart(data_amz)
     elif brand != '':
         data = pd.read_csv(f"../raw_data/sentiment_tw_{brand}.csv")
+        topic = topics['negative_topics'][f"{brand}_tw"]
     elif product != '':
         data = pd.read_csv(f"../raw_data/sentiment_amz_{product}.csv")
         data['date'] = pd.to_datetime(data['date'])
+        topic = topics['negative_topics'][f"{product}_amz"]
         fig3 = timeline_chart(data)
 
 
@@ -88,7 +84,7 @@ if st.button("Get report"):
 
 
     fig1 = pie_chart(data)
-    fig2 = word_cloud(topics_neg_sneakers)
+    fig2 = word_cloud(topic)
 
     # linecount
     line_count = st.slider('Select a line count', 1, 10, 3)
